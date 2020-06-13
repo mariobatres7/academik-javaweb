383f14f475bede0935aba23f26b292f41110f5e2
Mario Batres <mario.batres.gt@gmail.com>
2020-06-13 12:17:54 -0600

    Agregando código de Primer Servlet

---
+++ PrimerServlet/src/main/java/edu/academik/primerservlet/ImageServlet.java
@@ -0,0 +1,126 @@
+package edu.academik.primerservlet;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Mario Batres
+ */
+@WebServlet(name = "ImageServlet", urlPatterns = {"/image"})
+@MultipartConfig(location = "/tmp/academika")
+public class ImageServlet extends HttpServlet {
+
+    private static int contador = 0;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("image/jpeg");
+
+        String numeroStr = request.getParameter("numero");
+
+        if (numeroStr != null) {
+
+            contador++;
+
+            System.out.println(contador);
+
+            try (InputStream inputStream = new FileInputStream(
+                    "/tmp/academika/" + numeroStr + ".jpg")) {
+                byte[] bytes = inputStream.readAllBytes();
+
+                try (OutputStream outputStream = response.getOutputStream()) {
+                    outputStream.write(bytes);
+                }
+            }
+
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getParts().stream()
+                .forEach(part -> {
+                    try {
+
+                        Date date = new Date();
+                        part.write(date.getTime() + "-" + part.getSubmittedFileName());
+
+                    } catch (IOException ex) {
+                        Logger.getLogger(ImageServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                });
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HelloServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Archivo cargado satisfactoriamente</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ PrimerServlet/src/main/java/edu/academik/primerservlet/pojo/Producto.java
@@ -0,0 +1,29 @@
+package edu.academik.primerservlet.pojo;
+
+/**
+ *
+ * @author Mario Batres
+ */
+public class Producto {
+
+    private Long id;
+
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
